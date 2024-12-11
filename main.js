cf334a5fbf8d9e339757a73cebf55d095f6476b8
RomuloBlack <108672490+RomuloBlack@users.noreply.github.com>
2024-12-11 19:58:43 -0300

    r

--- main.js
+++ main.js
@@ -17,9 +17,9 @@ for (let i=0; i<botoes.length; i++) {
 
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-04-30T23:59:59");
-const tempoObjetivo2 = new Date("2024-05-10T23:59:59");
-const tempoObjetivo3 = new Date("2024-06-15T23:59:59");
+const tempoObjetivo1 = new Date("2024-12-13T23:59:59");
+const tempoObjetivo2 = new Date("2024-12-13T23:59:59");
+const tempoObjetivo3 = new Date("2024-12-13T23:59:59");
 const tempoObjetivo4 = new Date("2024-12-13T23:59:59");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4]
